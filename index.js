86e49d9aa68fb59144869a6f5eb5c40540b4fbb3
brice meunier <brice.meunier@etu.unilim.fr>
2019-06-15 19:40:12 +0200

    creating the html page

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
-});
-server.listen(8080);
+function onRequest(request,res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error,data) {
+    if (error) {
+      res.writeHead(404);
+      res.write('error 404');
+    }
+    else res.write(data);
+    res.end();
+  });
+}
+
+http.createServer(onRequest).listen(8080);
